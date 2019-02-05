982bfc70093d3f9f9582b415d542a078efc58dbd
ClayFlores <ClayFlores@users.noreply.github.com>
2019-02-04 23:53:47 -0800

    Project upload
    First hw project - making a rolling ball game

---
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    // late update runs after all items have been processed
+    void LateUpdate()
+    {
+        // camera is moved before each frame is displayed
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+    // need one version of update to check every frame for input and apply that input also
+    // Fixed update chosen b/c called before physics calcs
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
